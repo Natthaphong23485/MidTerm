a20d92eb33d4e6ae656050aee0420a4c9adb22cc
Natthaphong23485 <newjam2348582@gmail.com>
2024-08-18 17:48:11 +0700

    rebuild layout

---
+++ script.js
@@ -0,0 +1,14 @@
+// function adjustContainerClass() {
+//     const navbarContainer = document.getElementById('nav');
+  
+//     if (window.innerWidth >= 768) {
+//       navbarContainer.classList.remove('w-100');
+//       navbarContainer.classList.add('container-lg');
+//     } else {
+//       navbarContainer.classList.remove('container-lg');
+//       navbarContainer.classList.add('w-100');
+//     }
+//   }
+  
+//   window.addEventListener('resize', adjustContainerClass);
+//   window.addEventListener('load', adjustContainerClass);
